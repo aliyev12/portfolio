64110df4dc2afc52b1a055161e8dfe6d808e0315
Abdul Aliyev <aliyev_abdul@bah.com>
2020-01-01 16:53:03 -0500

    first commit

---
+++ src/data/config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  defaultTitle: 'Abdul Aliyev',
+  logo: 'https://aaliyev.com/wp-content/uploads/2019/03/mylogo-300dpi-180x180.png',
+  author: 'Abdul Aliyev',
+  url: 'https://aaliyev.com',
+  legalName: 'Abdul Aliyev',
+  defaultDescription: 'I’m Abdul and I’m a Software Developer!',
+  socialLinks: {
+    twitter: 'https://twitter.com/AbdulAliyev2',
+    github: 'https://github.com/aliyev12',
+    linkedin: 'https://www.linkedin.com/in/aliyevabdul',
+  },
+  googleAnalyticsID: 'UA-88875900-4',
+  themeColor: '#6b63ff',
+  backgroundColor: '#6b63ff',
+  social: {
+    twitter: '@AbdulAliyev2',
+  },
+  address: {
+    city: 'City',
+    region: 'Region',
+    country: 'Country',
+    zipCode: 'ZipCode',
+  },
+  contact: {
+    email: 'email',
+    phone: 'phone number',
+  },
+  foundingDate: '2018',
+  recaptcha_key: '6Lcs6lQUAAAAAEwhNH2IsobIe2csdda4TU3efpMN',
+};
