51dbbe80cfb9df0d211d886e2547b024d3863c6e
Abdul Aliyev <aaa7c4@gmail.com>
2020-01-01 22:57:37 -0500

    Add menu and pages

--- src/components/theme/Header/NavbarLinks/index.jsx
+++ src/components/theme/Header/NavbarLinks/index.jsx
@@ -1,13 +1,65 @@
-import React from 'react'
-import AnchorLink from 'react-anchor-link-smooth-scroll'
-import { Wrapper } from './styles'
+import React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
+import { Wrapper } from './styles';
 
-const NavbarLinks = ({ desktop }) => (
-	<Wrapper desktop={desktop}>
-		<AnchorLink href="#about">About</AnchorLink>
-		<AnchorLink href="#projects">Projects</AnchorLink>
-		<AnchorLink href="#contact">Contact</AnchorLink>
-	</Wrapper>
-)
+export const MENU_ITEMS = graphql`
+  query {
+    menuItems: allWordpressWpApiMenusMenusItems(
+      filter: { name: { eq: "Top Navbar Menu" } }
+    ) {
+      edges {
+        node {
+          items {
+            id: wordpress_id
+            title
+            href: url
+            slug: object_slug
+          }
+        }
+      }
+    }
+  }
+`;
 
-export default NavbarLinks
+const NavbarLinks = ({ desktop }) => {
+  const data = useStaticQuery(MENU_ITEMS);
+  const menuItems = formatMenu(data.menuItems);
+  console.log('menuItems = ', menuItems);
+
+  return (
+    <Wrapper desktop={desktop}>
+      {menuItems.map(menuItem => (
+        <AnchorLink href={menuItem.href} key={menuItem.id}>
+          {menuItem.title}
+        </AnchorLink>
+      ))}
+    </Wrapper>
+  );
+};
+
+export default NavbarLinks;
+
+function formatMenu(items) {
+  const menuItems = [];
+
+  if (items.edges && items.edges.length) {
+    const { node } = items.edges[0];
+    if (node && node.items && node.items.length) {
+      node.items.forEach(el => {
+        const item = {
+          id: '',
+          title: '',
+          href: '',
+          slug: '',
+        };
+        if (el.id) item.id = el.id;
+        if (el.title) item.title = el.title;
+        if (el.href) item.href = el.href;
+        if (el.slug) item.slug = el.slug;
+        menuItems.push(item);
+      });
+    }
+  }
+  return menuItems;
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,64 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 import { Layout, SEO } from 'components/common';
 import { Intro, Skills, Contact, Projects } from 'components/landing';
 
-export default () => (
-  <Layout>
-    <SEO />
-    <Intro />
-    <Projects />
-    <Skills />
-    <Contact />
-  </Layout>
-);
+export default ({ data }) => {
+  const pages = formatPages(data.pages);
+  console.log('data from index = ', pages);
+
+  return (
+    <Layout>
+      <SEO />
+      <Intro />
+      {pages.map(page => {
+        if (page.slug === 'about') return <Skills key={page.id} {...page} />;
+        if (page.slug === 'projects') return <Projects key={page.id} />;
+        if (page.slug === 'contact') return <Contact key={page.id} />;
+      })}
+    </Layout>
+  );
+};
+
+export const query = graphql`
+  query PAGES {
+    pages: allWordpressPage(sort: { fields: menu_order }) {
+      edges {
+        node {
+          id: wordpress_id
+          slug
+          title
+          content
+          order: menu_order
+        }
+      }
+    }
+  }
+`;
+
+function formatPages(pagesData) {
+  const formattedPages = [];
+
+  if (pagesData.edges && pagesData.edges.length) {
+    pagesData.edges.forEach(({ node }) => {
+      if (node) {
+        const page = {
+          id: '',
+          slug: '',
+          title: '',
+          content: '',
+          order: 0,
+        };
+        if (node.id) page.id = node.id;
+        if (node.slug) page.slug = node.slug;
+        if (node.title) page.title = node.title;
+        if (node.content) page.content = node.content.trim();
+        if (node.order) page.order = node.order;
+
+        formattedPages.push(page);
+      }
+    });
+  }
+
+  return formattedPages;
+}
