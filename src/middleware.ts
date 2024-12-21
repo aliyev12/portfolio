8d59c5c3c9bbac34368b34cca37357ef8e37c469
Abdul Aliyev <aaa7c4@gmail.com>
2024-12-21 00:13:34 -0500

    Add additional console logs to debug maintenance page

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,8 +8,16 @@ export const onRequest = defineMiddleware((context, next) => {
     "@@ in middleware and import.meta.env.MAINTENANCE_MODE = ",
     import.meta.env.MAINTENANCE_MODE
   );
+  console.log(
+    "@@ typeof import.meta.env.MAINTENANCE_MODE = ",
+    typeof import.meta.env.MAINTENANCE_MODE
+  );
 
   if (isMaintenance && context.url.pathname !== MAINTENANCE_PATH) {
+    console.log(
+      "@@ redirecting, context.url.pathname = ",
+      context.url.pathname
+    );
     return Response.redirect(new URL("/maintenance", context.url), 302);
   }
 
