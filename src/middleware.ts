4afdbeab751c3631fdae09965506abb73d075c9f
Abdul Aliyev <aaa7c4@gmail.com>
2024-12-21 00:14:02 -0500

    Add additional console logs to test

--- src/middleware.ts
+++ src/middleware.ts
@@ -15,7 +15,28 @@ export const onRequest = defineMiddleware((context, next) => {
     typeof import.meta.env.MAINTENANCE_MODE
   );
 
-  if (isMaintenance && context.url.pathname !== MAINTENANCE_PATH) {
+  const check1 =
+    import.meta.env.MAINTENANCE_MODE === "true" ||
+    import.meta.env.MAINTENANCE_MODE === true;
+
+  console.log("@@ check1 = ", check1);
+
+  console.log("@@ context.url.pathname = ", context.url.pathname);
+
+  const check2 =
+    (import.meta.env.MAINTENANCE_MODE === "true" ||
+      import.meta.env.MAINTENANCE_MODE === true) &&
+    context.url.pathname !== MAINTENANCE_PATH;
+
+  console.log("@@ check2 = ", check2);
+
+  //   console.log('context = ', context);
+
+  if (
+    (import.meta.env.MAINTENANCE_MODE === "true" ||
+      import.meta.env.MAINTENANCE_MODE === true) &&
+    context.url.pathname !== MAINTENANCE_PATH
+  ) {
     console.log(
       "@@ redirecting, context.url.pathname = ",
       context.url.pathname
