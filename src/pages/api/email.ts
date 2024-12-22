d2aa1530c6247b9323094836a60661a5038d44db
Abdul Aliyev <aaa7c4@gmail.com>
2024-12-21 23:02:49 -0500

    Add a test email route

---
+++ src/pages/api/email.ts
@@ -0,0 +1,14 @@
+import { SendEmail } from "@/services/sendEmail";
+import nodemailer from "nodemailer";
+
+export async function GET({ request }: any): Promise<Response> {
+  const sendEmail = new SendEmail(["john@email.com"]);
+
+  const sendResult = await sendEmail.send();
+
+  console.log("sendResult = ", sendResult);
+  return new Response(
+    JSON.stringify({ success: false, error: "An unexpected error occurred" }),
+    { status: 500 }
+  );
+}
---
+++ src/services/SendEmail.ts
@@ -0,0 +1,80 @@
+import nodemailer from "nodemailer";
+import type SMTPTransport from "nodemailer/lib/smtp-transport";
+import { getEntry, type CollectionEntry } from "astro:content";
+
+export class SendEmail {
+  recipients: string;
+  transporter: nodemailer.Transporter<
+    SMTPTransport.SentMessageInfo,
+    SMTPTransport.Options
+  >;
+
+  constructor(recipients: string[]) {
+    this.recipients = recipients.join(", ");
+
+    const MAIL_HOST = import.meta.env.MAIL_HOST;
+    const MAIL_PORT = import.meta.env.MAIL_PORT;
+    const MAIL_USERNAME = import.meta.env.MAIL_USERNAME;
+    const MAIL_PASSWORD = import.meta.env.MAIL_PASSWORD;
+
+    const transporter = nodemailer.createTransport({
+      host: MAIL_HOST,
+      port: MAIL_PORT,
+      auth: {
+        user: MAIL_USERNAME,
+        pass: MAIL_PASSWORD,
+      },
+    });
+
+    this.transporter = transporter;
+  }
+
+  async send() {
+    const { data }: CollectionEntry<"email"> = await getEntry(
+      "email",
+      "receipt"
+    )!;
+
+    console.log("data = ", data);
+
+    const info = await this.transporter.sendMail({
+      from: '"Maddison Foo Koch 👻" <maddison53@ethereal.email>', // sender address
+      to: this.recipients, // list of receivers
+      subject: "Howdy ✔", // Subject line
+      text: "Hello world?", // plain text body
+      html: "<b>Hello world?</b>", // html body
+    });
+
+    console.log("info = ", info);
+
+    /*
+   info =  {
+  accepted: [ 'john@email.com' ],
+  rejected: [],
+  ehlo: [
+    'SIZE 5242880',
+    'PIPELINING',
+    'ENHANCEDSTATUSCODES',
+    '8BITMIME',
+    'DSN',
+    'AUTH PLAIN LOGIN CRAM-MD5'
+  ],
+  envelopeTime: 33,
+  messageTime: 58,
+  messageSize: 640,
+  response: '250 2.0.0 Ok: queued',
+  envelope: { from: 'maddison53@ethereal.email', to: [ 'john@email.com' ] },
+  messageId: '<407a7eca-cbb4-fb44-472d-971ffa95a0d3@ethereal.email>'
+}
+    */
+
+    // prod
+    // 7ea32c1078cddd9550c0ee7a17d6b9b1
+
+    // demo
+    // 035a6115a18c38c51695203d3bd6e039
+
+    //
+    // 3008513feabc65bb0852c865bd5d8d6c
+  }
+}
