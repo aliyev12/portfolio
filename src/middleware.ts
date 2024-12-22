a56a2c8f6b988a7f8f93c502aad0267b73642fe3
Abdul Aliyev <aaa7c4@gmail.com>
2024-12-21 19:45:30 -0500

    Add a test log into middleware to test presence of env var

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,6 +4,10 @@ const MAINTENANCE_PATH = "/maintenance";
 
 export const onRequest = defineMiddleware((context, next) => {
   const isMaintenance = import.meta.env.MAINTENANCE_MODE === "true";
+  console.log(
+    "@@ in middleware and import.meta.env.MAINTENANCE_MODE = ",
+    import.meta.env.MAINTENANCE_MODE
+  );
 
   if (isMaintenance && context.url.pathname !== MAINTENANCE_PATH) {
     return Response.redirect(new URL("/maintenance", context.url), 302);
