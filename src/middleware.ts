ff220bb7bd73936eda2ef2d5f0686ce1316b0efc
Abdul Aliyev <aaa7c4@gmail.com>
2024-12-21 20:10:30 -0500

    Prevent infinite redirect by checking maintenance url

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,48 +3,50 @@ import { defineMiddleware } from "astro/middleware";
 const MAINTENANCE_PATH = "/maintenance";
 
 export const onRequest = defineMiddleware((context, next) => {
-  return Response.redirect(new URL("/maintenance", context.url), 302);
-
-  const isMaintenance =
-    import.meta.env.MAINTENANCE_MODE === "true" ||
-    import.meta.env.MAINTENANCE_MODE === true;
-  console.log(
-    "@@ in middleware and import.meta.env.MAINTENANCE_MODE = ",
-    import.meta.env.MAINTENANCE_MODE
-  );
-  console.log(
-    "@@ typeof import.meta.env.MAINTENANCE_MODE = ",
-    typeof import.meta.env.MAINTENANCE_MODE
-  );
-
-  const check1 =
-    import.meta.env.MAINTENANCE_MODE === "true" ||
-    import.meta.env.MAINTENANCE_MODE === true;
-
-  console.log("@@ check1 = ", check1);
-
-  console.log("@@ context.url.pathname = ", context.url.pathname);
-
-  const check2 =
-    (import.meta.env.MAINTENANCE_MODE === "true" ||
-      import.meta.env.MAINTENANCE_MODE === true) &&
-    context.url.pathname !== MAINTENANCE_PATH;
-
-  console.log("@@ check2 = ", check2);
-
-  //   console.log('context = ', context);
-
-  if (
-    (import.meta.env.MAINTENANCE_MODE === "true" ||
-      import.meta.env.MAINTENANCE_MODE === true) &&
-    context.url.pathname !== MAINTENANCE_PATH
-  ) {
-    console.log(
-      "@@ redirecting, context.url.pathname = ",
-      context.url.pathname
-    );
+  if (context.url.pathname !== MAINTENANCE_PATH) {
     return Response.redirect(new URL("/maintenance", context.url), 302);
   }
 
-  return next();
+  //   const isMaintenance =
+  //     import.meta.env.MAINTENANCE_MODE === "true" ||
+  //     import.meta.env.MAINTENANCE_MODE === true;
+  //   console.log(
+  //     "@@ in middleware and import.meta.env.MAINTENANCE_MODE = ",
+  //     import.meta.env.MAINTENANCE_MODE
+  //   );
+  //   console.log(
+  //     "@@ typeof import.meta.env.MAINTENANCE_MODE = ",
+  //     typeof import.meta.env.MAINTENANCE_MODE
+  //   );
+
+  //   const check1 =
+  //     import.meta.env.MAINTENANCE_MODE === "true" ||
+  //     import.meta.env.MAINTENANCE_MODE === true;
+
+  //   console.log("@@ check1 = ", check1);
+
+  //   console.log("@@ context.url.pathname = ", context.url.pathname);
+
+  //   const check2 =
+  //     (import.meta.env.MAINTENANCE_MODE === "true" ||
+  //       import.meta.env.MAINTENANCE_MODE === true) &&
+  //     context.url.pathname !== MAINTENANCE_PATH;
+
+  //   console.log("@@ check2 = ", check2);
+
+  //   //   console.log('context = ', context);
+
+  //   if (
+  //     (import.meta.env.MAINTENANCE_MODE === "true" ||
+  //       import.meta.env.MAINTENANCE_MODE === true) &&
+  //     context.url.pathname !== MAINTENANCE_PATH
+  //   ) {
+  //     console.log(
+  //       "@@ redirecting, context.url.pathname = ",
+  //       context.url.pathname
+  //     );
+  //     return Response.redirect(new URL("/maintenance", context.url), 302);
+  //   }
+
+  //   return next();
 });
