c21834219dfea5e509b1bcbe62ee4b8e0413b26a
Abdul Aliyev <aaa7c4@gmail.com>
2024-12-23 00:54:02 -0500

    Remove .vercel folder

--- .vercel/output/_functions/chunks/Separator_Q5zqhHer.mjs
+++
@@ -1,9 +0,0 @@
-import { a as createComponent, r as renderTemplate, m as maybeRenderHead } from './astro/server_BejUaFnn.mjs';
-import 'kleur/colors';
-import 'clsx';
-
-const $$Separator = createComponent(($$result, $$props, $$slots) => {
-  return renderTemplate`${maybeRenderHead()}<div class="flex flex-col justify-center items-center w-full my-8 md:my-20"> <div class="w-3/4 border-t border-t-foreground"></div> </div>`;
-}, "/Users/abdulaliyev/projects/portfolio/src/components/common/Separator.astro", void 0);
-
-export { $$Separator as $ };
