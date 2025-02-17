002118bd3ef32b6fa2b493bb8d70b2494c09d845
Abdul Aliyev <aaa7c4@gmail.com>
2025-02-17 01:55:51 -0500

    Remove partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,18 +4,14 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel";
 import sitemap from "@astrojs/sitemap";
-import partytown from "@astrojs/partytown";
+// Remove this line
+// import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     react({
       experimentalReactChildren: true,
-    }),
-    partytown({
-      config: {
-        forward: ["dataLayer.push"],
-      },
     }),
     tailwind({
       applyBaseStyles: false,
@@ -26,3 +22,32 @@ export default defineConfig({
   adapter: vercel(),
   site: "https://www.aaliyev.com",
 });
+
+// // @ts-check
+// import { defineConfig } from "astro/config";
+// import react from "@astrojs/react";
+// import tailwind from "@astrojs/tailwind";
+// import vercel from "@astrojs/vercel";
+// import sitemap from "@astrojs/sitemap";
+// import partytown from "@astrojs/partytown";
+
+// // https://astro.build/config
+// export default defineConfig({
+//   integrations: [
+//     react({
+//       experimentalReactChildren: true,
+//     }),
+//     partytown({
+//       config: {
+//         forward: ["dataLayer.push"],
+//       },
+//     }),
+//     tailwind({
+//       applyBaseStyles: false,
+//     }),
+//     sitemap(),
+//   ],
+//   output: "server",
+//   adapter: vercel(),
+//   site: "https://www.aaliyev.com",
+// });
